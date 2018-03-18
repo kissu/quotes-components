698907907c2fd9fca3fa37d27590095b93f536f1
Konstantin BIFERT <hello@kissu.io>
2018-03-18 23:20:32 +0100

    Add basic components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
     <div class="container">
-    
+        <div class="div">
+            <ul>
+                <li v-for="quote in quotes" :key="quote.id">{{ quote }}</li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        
+        data() {
+            return {
+                quotes: [ "I'm a Potato ! :3", "Hello, me too ! :)" ],
+                maxQuotes: 10
+            }
+        }
     }
 </script>
 
