5be69affe2cc8d291dc0d86adb797746c5bf5311
Konstantin BIFERT <hello@kissu.io>
2018-03-19 01:27:31 +0100

    Add removal, progressbar & alert if too much quotes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
 <template>
     <div class="container">
-        <p>This is main App parent</p>
-        <app-quote-grid :quotes="quotes"></app-quote-grid>
+        <p class='text-center bg-primary'>This is main App</p>
+        <br>
+        <div class="progress">
+          <div class="progress-bar progress-bar-striped active"
+            aria-valuenow='30' aria-valuemin='0' aria-valuemax='100'
+            :style="quotesPercentage">
+            {{ quotes.length * 10 }}%
+          </div>
+        </div>
+
+        <app-new-quote @quoteAdded='newQuote'></app-new-quote>
+        <hr>
+        {{ quotesPercentage }}
+        <app-quote-grid :quotes="quotes" @quoteDeleted='deleteQuote'></app-quote-grid>
+        <br>
+
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <div class='alert alert-info'>
+              Click on a Quote to delete it !
+            </div>
+          </div>
+
+        </div>
     </div>
 </template>
 
 <script>
   import QuoteGrid from './components/QuoteGrid.vue'
+  import NewQuote from './components/NewQuote.vue'
 
   export default {
     data() {
@@ -18,8 +41,26 @@
         maxQuotes: 10
       }
     },
+    methods: {
+      newQuote(quote) {
+        if (this.quotes.length > 9) {
+          alert('You reached the limit of the quotes, delete some pls.')
+        } else {
+          this.quotes.push(quote)
+        }
+      },
+      deleteQuote(index) {
+        this.quotes.splice(index, 1)
+      }
+    },
+    computed: {
+      quotesPercentage() {
+        return `width: ${this.quotes.length * 10}%`
+      }
+    },
     components: {
-      appQuoteGrid: QuoteGrid
+      appQuoteGrid: QuoteGrid,
+      appNewQuote: NewQuote,
     }
   }
 </script>
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="row">
+    <form>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+        <label for=""></label>
+        <textarea name="" class='form-control' id="" cols="30" rows="10" v-model='quote'></textarea>
+      </div>
+
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+        <br>
+        <button class='btn btn-success' @click.prevent='createNew'>Add quote</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        quote: ''
+      }
+    },
+    methods: {
+      createNew() {
+        this.$emit('quoteAdded', this.quote)
+        this.quote = ''
+      }
+    }
+  }
+</script>
