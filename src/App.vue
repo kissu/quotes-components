96fa48b482983faed74c639d755b1d5b06f278e4
Konstantin BIFERT <hello@kissu.io>
2018-03-21 00:23:57 +0100

    Project done

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,7 @@
     <div class="container">
         <p class='text-center bg-primary'>This is main App</p>
         <br>
-        <div class="progress">
-          <div class="progress-bar progress-bar-striped active"
-            aria-valuenow='30' aria-valuemin='0' aria-valuemax='100'
-            :style="quotesPercentage">
-            {{ quotes.length * 10 }}%
-          </div>
-        </div>
+        <app-progress-bar :quoteCount='this.quotes.length'></app-progress-bar>
 
         <app-new-quote @quoteAdded='newQuote'></app-new-quote>
         <hr>
@@ -30,6 +24,7 @@
 <script>
   import QuoteGrid from './components/QuoteGrid.vue'
   import NewQuote from './components/NewQuote.vue'
+  import ProgressBar from './components/ProgressBar.vue'
 
   export default {
     data() {
@@ -43,8 +38,8 @@
     },
     methods: {
       newQuote(quote) {
-        if (this.quotes.length > 9) {
-          alert('You reached the limit of the quotes, delete some pls.')
+        if (this.quotes.length > this.maxQuotes) {
+          return alert('You reached the limit of the quotes, delete some pls.')
         } else {
           this.quotes.push(quote)
         }
@@ -61,6 +56,7 @@
     components: {
       appQuoteGrid: QuoteGrid,
       appNewQuote: NewQuote,
+      appProgressBar: ProgressBar,
     }
   }
 </script>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="progress">
+    <div class="progress-bar progress-bar-striped active"
+      aria-valuenow='30' aria-valuemin='0' aria-valuemax='100'
+      :style="{width: (quoteCount) * 10 + '%'}">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['quoteCount', 'maxQuotes']
+  }
+</script>
